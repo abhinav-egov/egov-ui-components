c2afbe45d934b86963ed8d38116ac33fb22f537b
saurabh-egov <saurabh.s@egovernments.org>
2020-11-03 17:07:20 +0530

    file structure changes

--- example/src/components/CardCaption.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CardCaption = (props) =>{
-    return(
-        <label className="card-caption">{props.children}</label>
-    )
-}
-
-export default CardCaption
--- example/src/components/CardText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CardText = (props) =>{
-    return(
-        <p className="card-text">{props.children}</p>
-    )
-}
-
-export default CardText
